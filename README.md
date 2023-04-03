85f2196388acecd4d6eb9b93123ed94a57cbbc58
Burritosasquash <luke_sammut17@hotmail.com>
2023-04-03 23:35:44 +1000

    final changes before final submission, making slight changes to code and commenting out println's aswell as adding comments in the code and adding tutorial in the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-this is the README file
+in this repository you will find my stage 1 project containing a client side program that connects to the application ds-server.
+
+please see the following steps to connect the file projectclient.class to the server and run the program:
+
+step 1:
+start the program ds-server locally in a terminal window with the command "./ds-server -c 'your-sample-file.xml' -v brief -n" 
+the -n is important as it enables using newline at the end of each message and cannot run our client without it.
+
+step 2: run the command "java projectclient" in another terminal window so that you can see the results on either end.
+
+step 3: observe as the program runs.
+
+
+if you want to compile the file projectclient.java yourself please run "javac projectclient.java"
+
+if you would like to run the code without compiling into class please use the following command on step 2: "java projectclient.java"
--- projectclient.java
+++ projectclient.java
@@ -3,63 +3,63 @@ import java.net.*;
 public class projectclient {
 public static void main(String[] args) {
 try{
+
+// the below section starts the connection between the server and the client
+// the below section also creates our readers and output streams for communication
+
 Socket s=new Socket("127.0.0.1", 50000);
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-DataInputStream dis=new DataInputStream(s.getInputStream());
 BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-
 String username = System.getProperty("user.name");
+
+//the below is made so that initiation between client and server can be made
 
 dout.write(("HELO\n").getBytes());
 dout.flush();
 String str = (String)in.readLine();
-System.out.println("server says: "+str);
+//System.out.println("server says: "+str);
 dout.write(("AUTH luke\n").getBytes());
 dout.flush();
 str = (String)in.readLine();
-System.out.println("server says "+str);
+//System.out.println("server says "+str);
 
-//getting jobs to work with
+//below is made so that the first job can be recieved from the server
 
-	dout.write(("REDY\n").getBytes());
-	dout.flush();
-	str = (String)in.readLine();
-	System.out.println("server says "+str);
-	String[] serverJobs = str.split(" ");
-	int jobNum = Integer.parseInt(serverJobs[2]);
+dout.write(("REDY\n").getBytes());
+dout.flush();
+str = (String)in.readLine();
+//System.out.println("server says "+str);
+String[] serverJobs = str.split(" ");
+int jobNum = Integer.parseInt(serverJobs[2]);
 
-
-//get server info
+//below indicates how we get our server information 
+//once we have this it does not have to be done again
 
 dout.write(("GETS All\n").getBytes());
 dout.flush();
 str = (String)in.readLine();
-System.out.println("server says "+str);
+//System.out.println("server says "+str);
 String[] serverDatalist = str.split(" ");
 dout.write(("OK\n").getBytes());
 dout.flush();
 
 int serverCount = Integer.parseInt(serverDatalist[1]);
-int[] serverNumArr = new int[serverCount];
 int[] serverCoreArr = new int[serverCount];
 int largestServerId = 0;
-int[] serverIdArr = new int[serverCount];
 String[] serverTypeArr = new String[serverCount];
 
-System.out.println(serverCount);
+//System.out.println(serverCount);
 
-//finding all servers
+//putting all server information needed into arrays and values so that they can be referenced later
 
 for(int i=0; i< serverCount; i++){
 
 	str = (String)in.readLine();
-	System.out.println("server says "+str);
+	//System.out.println("server says "+str);
 	String[] serverList = str.split(" ");
-
 	serverCoreArr[i] = Integer.parseInt(serverList[4]);
 	serverTypeArr[i] = serverList[0];
-	serverIdArr[i] = Integer.parseInt(serverList[1]);
 	largestServerId = Integer.parseInt(serverList[1]);
 
 }
@@ -68,6 +68,8 @@ dout.write(("OK\n").getBytes());
 dout.flush();
 
 //finding largest server type || first server to have largest amount of cores
+//this is done so that we can implement the LRR algorithm into the scheduling loop
+
 int largestCoreServer = 0;
 String largestServerType = null;
 for(int i = 0; i < serverCount; i++){
@@ -77,42 +79,33 @@ for(int i = 0; i < serverCount; i++){
 	}
 }
 
+//System.out.println("highest core count: "+largestCoreServer);
+//System.out.println("server type with highest cores: "+largestServerType);
 
-System.out.println("highest core count: "+largestCoreServer);
-System.out.println("server type with highest cores: "+largestServerType);
-System.out.println("serverId: "+serverIdArr[serverCount-1]);
-
-//schedule first job now knowing the largest server sizes
+//scheduling the first job from the begining of the server connection 
+//now that we know what server it should be tasked to
 
 int j = 0;
 
-        jobNum = Integer.parseInt(serverJobs[2]);
-        dout.write(("SCHD "+jobNum+" "+largestServerType+" "+0+"\n").getBytes());
-        dout.flush();
-	j++;
-
+jobNum = Integer.parseInt(serverJobs[2]);
+dout.write(("SCHD "+jobNum+" "+largestServerType+" "+j+"\n").getBytes());
+dout.flush();
+j++;
 
 //begin loop for redying, getting jobs and job information
+//this loop ensures that once we are getting jobs we are scheduling them directly into their correct server according to the LRR algorithm
 
 int jobCount = 0;
-
 while(!str.contains("NONE")){
-	if(str.contains("NONE")){
-		break;
-	}
-
         str = (String)in.readLine();
-        System.out.println("server says "+str);
-
-
-//getting new jobs in the loop and queuing immediately
-        
+        //System.out.println("server says "+str);
 	serverJobs = str.split(" ");
+	
 	if(str.contains("JCPL")){
 	        dout.write(("REDY\n").getBytes());
                 dout.flush();
-
 	}
+	
 	if(str.contains("JOBN")){
 		if(j == largestServerId+1){
 			j = 0;
@@ -128,12 +121,16 @@ while(!str.contains("NONE")){
 	}
 }
 
+//once the loop finishes that will indicate that we have assigned all jobs 
+//meaning we can initialte the quitting of the client server connection and end the program
+
 dout.write(("QUIT\n").getBytes());
 dout.flush();
 str = (String)in.readLine();
-System.out.println("server says "+str);
-System.out.println("server connection ended");
+//System.out.println("server says "+str);
+//System.out.println("server connection ended");
 dout.close();
+in.close();
 s.close();
 }catch(Exception e){System.out.println(e);}
 }
