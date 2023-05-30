cfb356fd92d5babb4a05e5c8b6e7b5ef68057ac8
Burritosasquash <luke_sammut17@hotmail.com>
2023-05-30 19:38:01 +1000

    adding final commit before submission

--- S2Client.java
+++ S2Client.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-public class projectclient {
+public class S2Client {
 public static void main(String[] args) {
 try{
 Socket s=new Socket("127.0.0.1", 50000);
@@ -25,21 +25,21 @@ System.out.println("server says "+str);
 	dout.write(("REDY\n").getBytes());
 	dout.flush();
 	str = (String)in.readLine();
-	System.out.println("server says "+str);
+	//System.out.println("server says "+str);
 	String[] serverJobs = str.split(" ");
 	int jobNum = Integer.parseInt(serverJobs[2]);
 	int jobCore = Integer.parseInt(serverJobs[4]);
 	int jobMem = Integer.parseInt(serverJobs[5]);
 	int jobDisk = Integer.parseInt(serverJobs[6]);
 
-System.out.println("Job"+jobNum+" "+jobCore+" "+jobMem+" "+jobDisk);
+//System.out.println("Job"+jobNum+" "+jobCore+" "+jobMem+" "+jobDisk);
 
 //get server info via capable
 
 dout.write(("GETS Avail "+jobCore+" "+jobMem+" "+jobDisk+"\n").getBytes());
 dout.flush();
 str = (String)in.readLine();
-System.out.println("server says "+str);
+//System.out.println("server says "+str);
 String[] serverDatalist = str.split(" ");
 dout.write(("OK\n").getBytes());
 dout.flush();
@@ -51,14 +51,14 @@ int largestServerId = 0;
 int[] serverIdArr = new int[serverCount];
 String[] serverTypeArr = new String[serverCount];
 
-System.out.println(serverCount);
+//System.out.println(serverCount);
 
 //finding all servers inputting server data into variables
 
 for(int i=0; i< serverCount; i++){
 
 	str = (String)in.readLine();
-	System.out.println("server says "+str);
+	//System.out.println("server says "+str);
 	String[] serverList = str.split(" ");
 
 	serverCoreArr[i] = Integer.parseInt(serverList[4]);
@@ -76,7 +76,7 @@ dout.flush();
         dout.write(("SCHD "+jobNum+" "+serverTypeArr[0]+" "+serverIdArr[0]+"\n").getBytes());
         dout.flush();
         str = (String)in.readLine();
-        System.out.println("server says "+str);
+        //System.out.println("server says "+str);
         
         
 while(!str.contains("NONE")){
@@ -94,15 +94,15 @@ while(!str.contains("NONE")){
 		jobMem = Integer.parseInt(serverJobs[5]);
 		jobDisk = Integer.parseInt(serverJobs[6]);
 		
-		System.out.println("Job"+jobNum+" "+jobCore+" "+jobMem+" "+jobDisk);
+		//System.out.println("Job"+jobNum+" "+jobCore+" "+jobMem+" "+jobDisk);
 		
 		dout.write(("GETS Avail "+jobCore+" "+jobMem+" "+jobDisk+"\n").getBytes());
 		dout.flush();
 		str = (String)in.readLine();
-		System.out.println("server says "+str);
+		//System.out.println("server says "+str);
 		serverDatalist = str.split(" ");
 		serverCount = Integer.parseInt(serverDatalist[1]);
-		System.out.println(serverCount);
+		//System.out.println(serverCount);
 		
 		
 		
@@ -111,17 +111,17 @@ while(!str.contains("NONE")){
 			dout.write(("OK\n").getBytes());
 			dout.flush();
 			str = (String)in.readLine();
-			System.out.println("server says "+str);
-			System.out.println("gets Capable encountered");
-			System.out.println("server says "+str);
+			//System.out.println("server says "+str);
+			//System.out.println("gets Capable encountered");
+			//System.out.println("server says "+str);
 			dout.write(("GETS Capable "+jobCore+" "+jobMem+" "+jobDisk+"\n").getBytes());
 			dout.flush();
-			System.out.println("gets Capable activated");
+			//System.out.println("gets Capable activated");
 			str = (String)in.readLine();
-			System.out.println("server says "+str);
+			//System.out.println("server says "+str);
 			serverDatalist = str.split(" ");
 			serverCount = Integer.parseInt(serverDatalist[1]);
-			System.out.println(serverCount);
+			//System.out.println(serverCount);
 		}
 		
 		dout.write(("OK\n").getBytes());
@@ -134,7 +134,7 @@ while(!str.contains("NONE")){
 		for(int i=0; i < serverCount; i++){
 
 			str = (String)in.readLine();
-			System.out.println("server says "+str);
+			//System.out.println("server says "+str);
 			String [] serverList = str.split(" ");
 			AvailCoreArr[i] = Integer.parseInt(serverList[4]);
 			AvailTypeArr[i] = serverList[0];
@@ -150,7 +150,7 @@ while(!str.contains("NONE")){
 		dout.write(("SCHD "+jobNum+" "+AvailTypeArr[0]+" "+AvailIdArr[0]+"\n").getBytes());
         	dout.flush();
         	str = (String)in.readLine();
-        	System.out.println("server says "+str);
+        	//System.out.println("server says "+str);
 	}
 	if(str.contains("OK")){
         	dout.write(("REDY\n").getBytes());
